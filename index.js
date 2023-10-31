e9aa7b17be9eddb75398ce6804c3ee5e791d9ae7
Patrick Justice <pjustice@caldwellschools.com>
2023-10-31 15:33:37 -0400

    second commit

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 const express= require ('express');
 const defaultRouter = require('./default.routes');
 const studentRouter = require('./student.routes')
+const studentController = require("../express-demo-app/controllers/student.controller")
 const app = express();
-const port = 5000
+const port = 5010
 
 
 app.use ('/',defaultRouter)
 app.use ('/api',studentRouter)
+app.use(express.json());
+
+app.post('/students', studentController.add);
+app.put('/students/:id', studentController.update);
+app.delete('/students/:id', studentController.delete);
 
 
 app.listen (port, function (){
--- student.routes.js
+++ student.routes.js
@@ -1,10 +1,24 @@
-const express = require('express')
-const router = express.Router()
-const studentController = require('./student.controller')
 
-router.get('/students', studentController.findAll)
-router.get('/students/:id', studentController.findById)
+//require express and use it to create our router from express.Router()
+const express = require("express");
+const router = express.Router();
+  
+const studentController = require("../express-demo-app/controllers/student.controller")
+
+//get all the students
+router.get("/students", studentController.findAll)
+
+//get a student by id 
+router.get("/students/:id", studentController.findById)
+
+//add a student
+router.post("/students", studentController.add)
 
 
+//updatge a student by id 
+router.put("/students/:id", studentController.update)
 
-module.exports= router
+//updatge a student by id 
+router.delete("/students/:id", studentController.delete)
+
+module.exports = router;
